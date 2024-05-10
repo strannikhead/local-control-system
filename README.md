7b839b5a0b677131f440d4a93edd27e32c3fb02d
strannikhead <144272555+strannikhead@users.noreply.github.com>
2024-05-10 15:31:00 +0500

    add setup and new readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+<h3>Local Remote System</h3>
+<p>For setup application open terminal/cmd in directory with cvs files and input "python3 setup.py install"</p>
+<p>After installing you can use application from any folder</p>
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+import platform
+
+
+def add_to_path_if_not_exists(directory):
+    paths = os.environ['PATH'].split(os.pathsep)
+    if directory not in paths:
+        os.environ['PATH'] += os.pathsep + directory
+
+
+def add_to_profile_if_not_exists(directory):
+    system = platform.system()
+    if system == 'Linux' or system == 'Darwin':
+        profile_path = os.path.expanduser('~/.bashrc')
+        if not os.path.exists(profile_path):
+            profile_path = os.path.expanduser('~/.bash_profile')
+        if os.path.exists(profile_path):
+            with open(profile_path, 'a') as profile:
+                profile.write('\nexport PATH="{}:$PATH"\n'.format(directory))
+    elif system == 'Windows':
+        os.environ['PATH'] += os.pathsep + directory
+
+
+app_installation_path = ''
+
+add_to_path_if_not_exists(app_installation_path)
+add_to_profile_if_not_exists(app_installation_path)
+
+setup(
+    name='cvs',
+    version='1.0',
+    py_modules=['cvs', 'utils'],
+    entry_points={
+        'console_scripts': [
+            'cvs=cvs:cli'
+        ],
+    },
+)
